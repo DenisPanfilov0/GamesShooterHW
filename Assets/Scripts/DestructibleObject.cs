7cca9d5f415eaeff40d1bd947f759ad9998b66e4
DenisPanfilov0 <denispanfilov119@gmail.com>
2023-02-12 19:56:02 +0300

    Add Weapon

---
+++ Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DestructibleObject : MonoBehaviour
+
+    {
+        [SerializeField] private float hpInitial = 100;
+        [SerializeField] private float hpCurrent = 100;
+
+        internal void ReceiveDamage(float damage)
+        {
+            hpCurrent -= damage;
+            // можно писать так hpCurrent = hpCurrent - 30f;
+
+            if (hpCurrent <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private float force = 10f;
+    [SerializeField] private float damage = 1f;
+    [SerializeField] private GameObject impactPrefab;
+    [SerializeField] private Transform shootPoint;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(shootPoint.position, shootPoint.forward, out var hit))
+            {
+                print(hit.transform.gameObject.name);
+
+                Instantiate(impactPrefab, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up));
+
+                var destructible = hit.transform.GetComponent<DestructibleObject>();
+                if (destructible != null)
+                {
+                    destructible.ReceiveDamage(damage);
+                }
+
+                var rigidbody = hit.transform.GetComponent<Rigidbody>();
+                if (rigidbody != null)
+                {
+                    rigidbody.AddForce(shootPoint.forward * force, ForceMode.Impulse);
+                    //hit.transform.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(shootPoint.position, shootPoint.forward * 9999);
+    }
+}
